aac1845963a6a60034169478b76c929f354d39b4
papunjay kumar <kumarpapunjay@gmail.com>
2021-03-30 21:42:03 +0530

    [papunjay].add teprature conversion code..

---
+++ Basic_Code/teprature_conversion_tabels.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int c;
+    cout<<"F  --------  C"<<endl;
+    for(int f=0;f<300;f=f+20)
+    {
+        c=(5*(f-32))/9;
+       
+        cout<<f<<"----"<<c<<endl;
+    }
+
+}
